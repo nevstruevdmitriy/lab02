200d9c3d0bf31b022c39c5fc38fc9a7e14507a22
dimkanevstruev <nevstruev.dmitriy@mail.ru>
2020-03-26 12:32:23 +0300

    add comens

--- sources/hello_word.cpp
+++ sources/hello_word.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main () {
-	std::string user_name;
-	std::cin >> user_name;
-	std::cout << "hello word from" << user_name;
+	std::string user_name; // name user \\ имя пользователя
+	std::cin >> user_name; // ask \\ спросим
+	std::cout << "hello word from" << user_name; // say hello \\ поздоровываемся
 	return 0;
 }
