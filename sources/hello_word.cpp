82ac09e6fc0dc023c56f6f458fa51e492ed1f2c2
Nevstruev Dmitriy <nevstruev.dmitriy@mail.ru>
2020-03-26 12:46:45 +0300

    Update hello_word.cpp

--- sources/hello_word.cpp
+++ sources/hello_word.cpp
@@ -3,7 +3,7 @@
 
 int main () {
 	std::string user_name; // name user \\ имя пользователя
-	std::cin >> user_name; // ask \\ спросим
+	std::cin >> user_name; // ask a name \\ познакомимся
 	std::cout << "hello word from" << user_name; // say hello \\ поздоровываемся
 	return 0;
 }
