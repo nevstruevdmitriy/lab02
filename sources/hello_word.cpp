3a73505132fdf754da4ac9e4faf343a8d6d5105f
dimkanevstruev <nevstruev.dmitriy@mail.ru>
2020-03-26 12:25:40 +0300

    add getting name and forgotten one example

--- sources/hello_word.cpp
+++ sources/hello_word.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
 int main () {
-	cout << "hello word!";
+	string user_name;
+	cin >> user_name;
+	cout << "hello word from" << user_name;
 	return 0;
 }
